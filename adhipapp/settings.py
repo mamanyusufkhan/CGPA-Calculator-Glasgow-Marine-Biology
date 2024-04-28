9033838cd3dd23ffbe260e0966ae5bc5d4cf401f
mamanyusufkhan <mamanyusufkhan@gmail.com>
2024-04-29 01:41:42 +0600

    updated Allowed Hosts

--- adhipapp/settings.py
+++ adhipapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n#50d)y1@z@jk@a#6-$j74fetf*-evq*$t0fj-5iwr*tfi!4$x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
